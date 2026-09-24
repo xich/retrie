5114534e1943a30e871f6f3d0a0bf79b203ca73d
Simon Horlick <simonhorlick@gmail.com>
2026-09-24 11:48:18 +0800

    Keep annotations when reassociating infix constructor patterns
    fixOnePat built the new outer node from the old inner span, which lacks
    the pattern's entry delta, comments and trailing comma. Rebuild it from
    the outer span and strip comments from the new inner node, as fixOneExpr
    does.

--- Retrie/ExactPrint.hs
+++ Retrie/ExactPrint.hs
@@ -154,12 +154,12 @@ fixOneExpr env (L l2 (OpApp x2 ap1@(L _ (OpApp x1 x op1 y)) op2 z))
 fixOneExpr _ e = return e
 
 fixOnePat :: Monad m => FixityEnv -> LPat GhcPs -> TransformT m (LPat GhcPs)
-fixOnePat env (dLPat -> Just (L l2 (ConPat ext2 op2 (InfixCon (dLPat -> Just ap1@(L l1 (ConPat ext1 op1 (InfixCon x y)))) z))))
+fixOnePat env (dLPat -> Just (L l2 (ConPat ext2 op2 (InfixCon (dLPat -> Just ap1@(L _ (ConPat ext1 op1 (InfixCon x y)))) z))))
   | associatesRight (lookupOpRdrName op1 env) (lookupOpRdrName op2 env) = do
-    let ap2' = L l2 (ConPat ext2 op2 (InfixCon y z))
+    let ap2' = L (stripComments l2) (ConPat ext2 op2 (InfixCon y z))
     (_, ap2'_0) <- swapEntryDPT ap1 ap2'
     rhs <- fixOnePat env (cLPat ap2'_0)
-    return $ cLPat $ L l1 (ConPat ext1 op1 (InfixCon x rhs))
+    return $ cLPat $ L l2 (ConPat ext1 op1 (InfixCon x rhs))
 fixOnePat _ e = return e
 
 -- TODO: move to ghc-exactprint
